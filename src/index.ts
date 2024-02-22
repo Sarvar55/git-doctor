bc0d6e79b9587db5131a2871a196c9d416d5da01
Sarvar55 <sarvar.musazade@gmail.com>
2024-02-22 17:03:59 +0300

    prettier

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ import { translateCommit } from './utils/translate-commit'
 
 async function de() {
 	const commit = await translateCommit('yeni bir dosya eklendi')
+
+	// lo
+
+	localStorage.setItem()
+
+	///
+
+	for (let index = 0; index < array.length; index++) {
+		const element = array[index]
+	}
 	console.log(commit)
 }
 
