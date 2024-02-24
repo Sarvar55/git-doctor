a5b15f9649679c09e229417bc716137d6ec45061
Sarvar55 <sarvar.musazade@gmail.com>
2024-02-24 17:13:14 +0300

    insalled config and commander packages

--- build/index.js
+++ build/index.js
@@ -1,17 +1,11 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import { translateCommit } from './utils/translate-commit.js';
-function de() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const commit = yield translateCommit('yeni bir dosya eklendi');
-        console.log(commit);
-    });
+async function de() {
+    const commit = await translateCommit('yeni bir dosya eklendi');
+    console.log(commit);
 }
+function greet(de) {
+    console.log(de);
+    console.log();
+}
+greet('d');
 de();
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { translateCommit } from './utils/translate-commit'
-
-async function de() {
-	const commit = await translateCommit('yeni bir dosya eklendi')
-	console.log(commit)
-}
-
-de()
