3bd4a66563860a35d3c5c7f4e782c95ca5e90c97
Sarvar55 <sarvar.musazade@gmail.com>
2024-02-26 14:58:49 +0300

    Fix typo in logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,6 +4,7 @@ import chalk from 'chalk'
 export const logger = {
 	info: (message: string) => outro(chalk.blue(message)),
 	success: (message: string) => outro(chalk.green(message)),
-	warining: (message: string) => outro(chalk.yellow(message)),
-	error: (message: string) => outro(chalk.red(message)),
+	warning: (message: string) => outro(chalk.yellow(message)),
+	error: (message?: string) =>
+		outro(chalk.red(message ? message : 'An error occurred')),
 }
