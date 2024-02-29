41f055ca8da555e5d5f5c2421f1062d16aafb1a1
Sarvar55 <sarvar.musazade@gmail.com>
2024-02-29 23:20:17 +0300

    Refactor: Remove unnecessary emoji prompts from commit message generation

--- src/utils/prompts.ts
+++ src/utils/prompts.ts
@@ -1,5 +1,4 @@
 import { APP_CONSTANTS, ConfigManager } from '../config/config'
-import { commitTypesWithDesc } from './constants'
 
 const config = new ConfigManager()
 const language: string =
@@ -7,17 +6,13 @@ const language: string =
 const hasEmoji: boolean = !!config.get(APP_CONSTANTS.hasEmoji) || false
 
 export const generatePrompt = (diff: string) => {
-	const emojiPrompt = `
-	- Use GitHub-supported emojis at the beginning of your commit message.
-	  ${commitTypesWithDesc()}
-	`
 	const prompt = `
 	- I want you to act as a commit message generator.
 	- I will give you git diff file, and your job is to convert it into useful commit message .
 	- Use this language ${language} for the write commit message pay attention to this.
 	- ${
 		hasEmoji
-			? emojiPrompt
+			? '- Use GitHub-supported emojis at the beginning of your commit message.'
 			: '- Do not preface the commit with any emoji or symbol.'
 	}
 	- Do not preface the commit with anything, use the present tense, return the full sentence, and use the conventional commits specification.
@@ -25,7 +20,7 @@ export const generatePrompt = (diff: string) => {
 	- Summarize this git diff into a useful, 10 words commit message.
 	 ${diff}
     Examples:
-      "The commit message will be placed here according to all changes in the project."
+      "${hasEmoji ? 'for example If you added a new feature, add this emoji ✨feat: If it is something else then add emoji accordingly' : ''} The commit message will be placed here according to all changes in the project."
     
 	-  Do not write any explanations or other words, just reply with the commit message.  
 	-  Do not include any additional text or explanations.
