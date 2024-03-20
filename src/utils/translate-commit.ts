b9919879fceb9520d210bd884bf1abab079e6274
Sarvar55 <sarvar.musazade@gmail.com>
2024-03-20 17:46:57 +0300

    feat: translate commit messages

--- src/utils/translate-commit.ts
+++ src/utils/translate-commit.ts
@@ -6,7 +6,6 @@ const config = new ConfigManager()
 export const translateCommit = async (commitmessage: string) => {
 	const sourceLang = config.get(APP_CONSTANTS.source_lang) ?? 'tr'
 	const targetLang = config.get(APP_CONSTANTS.targetLang) ?? 'en'
-
 	try {
 		const response = await axios.get(
 			`https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURI(commitmessage)}`
